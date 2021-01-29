778986b676b3b1d06ce2783da3aaa4fcf33fc755
mesmith454 <mesmith454@gmail.com>
2021-01-28 18:24:32 -0800

    IT LIVES

--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -19,23 +19,21 @@ class CommandLineInterface
     end
 
     def find_item_by_name(name)#r
-        puts Item.find_by(name)
+         Item.find_by(name: name)
     end
 
     def rename_item(name, new_name)#u
-        find_item_by_name
-        item.name = new_name
-        item.save
+        selected_item = find_item_by_name(name)
+        selected_item.update(name: new_name)
     end
 
     def add_item(user_id, item_name)
-        Inventory.new
-        inventory.save
+        Inventory.create
     end
 
-    def drop_item(user_id, item_id) #d
-        Inventory.select do |uid| uid = user_id
-            Inventory.delete(item_id)
+    def drop_item(user_id, item_name) #d
+        Inventory.where(user_id: user_id) do
+            Inventory.all.find_by(name: item_name).delete
         end
     end
 
@@ -46,55 +44,54 @@ class CommandLineInterface
         puts "ADD item"
         puts "RENAME item"
         puts "DROP item"
+        puts "EXIT"
     end
 
     def greet
         puts "Welcome to the Inventory Manager"
-       
-        selection_cycle
-        ans = gets.chomp
-
-        if ans == "CREATE"
-            puts "Please enter a name:"
-            name = gets.chomp
-            new_char(name)
+       loop do
             selection_cycle
             ans = gets.chomp
-        elsif ans == "VIEW_ALL"
-            puts Item.all.inspect
-            selection_cycle
-            ans = gets.chomp
-        elsif ans == "VIEW_INV"
-            puts "Please input user id:"
-            uid = gets.chomp
-            view_inventory(uid)
-            selection_cycle
-            ans = gets.chomp
-        elsif ans == "ADD"
-            puts "Please input a user id:"
-            # uida = gets.chomp
-            # puts "Please input an item id:"
-            # iida = gets.chomp
-            # add_item(uida, iida)
-            # selection_cycle
-            # ans = gets.chomp
-        elsif ans == "RENAME"
-            puts "What item would you like to rename?"
-            item_name = gets.chomp
-            puts "What would you like to rename #{item_name} to?"
-            new_name = gets.chomp
-            rename_item(item_name, new_name)
-            puts "The item formerly known as #{item_name} is now named #{new_name}."
-            selection_cycle
-            ans = gets.chomp
-        elsif ans == "DROP"
-            puts "Please enter a user id:"
-            uidd = gets.chomp
-            puts "Please enter an item id:"
-            iidd = gets.chomp
-            Inventory.delete(uidd, iidd)
-            selection_cycle
-            ans = gets.chomp
+            
+            if ans == "CREATE"
+                puts "Please enter a name:"
+                name = gets.chomp
+                new_char(name)
+            
+            elsif ans == "VIEW_ALL"
+                puts Item.all.inspect
+                
+            elsif ans == "VIEW_INV"
+                puts "Please input user id:"
+                uid = gets.chomp
+                view_inventory(uid)
+                
+            elsif ans == "ADD"
+                puts "Please input a user id:"
+                uida = gets.chomp
+                puts "Please input an item id:"
+                iida = gets.chomp
+                add_item(uida, iida)
+                view_inventory(uida)
+                
+            elsif ans == "RENAME"
+                puts "What item would you like to rename?"
+                item_name = gets.chomp
+                puts "What would you like to rename #{item_name} to?"
+                new_name = gets.chomp
+                rename_item(item_name, new_name)
+                puts "The item formerly known as #{item_name} is now named #{new_name}."
+               
+            elsif ans == "DROP"
+                puts "Please enter a user id:"
+                uidd = gets.chomp
+                puts "Please enter an item id:"
+                iidd = gets.chomp
+                drop_item(uidd, iidd)
+                view_inventory(uidd)
+            else
+                exit
+            end
         end
     end
 end
